067acebb20dae90d6d0e2de1a3bddb92b6fe5953
Đỗ Thị Trang <trangdt@notavn.com>
2024-11-25 00:31:06 +0700

    feat: call api

--- BE/README.md
+++ BE/README.md
@@ -1,36 +1,39 @@
 # Requirements
+
 - Python 3
 - Flask
 
 # Local Run
 
 ## Windows (need to have python 3.9)
+
 Create a virtual env (mandatory): https://docs.python-guide.org/dev/virtualenvs/#virtualenvironments-ref
-   pip install virtualenv
-   virtualenv venv
-   .\venv\Scripts\activate
+pip install virtualenv
+virtualenv venv
+.\venv\Scripts\activate
 
 Install requirements:
-   pip install -r requirements.txt
+pip install -r requirements.txt
 
 Run the Application:
-   flask run
-   The app will be running at http://127.0.0.1:5000.
+flask run
+The app will be running at http://127.0.0.1:5000.
 
 Test the API:
-   - Drag or Choose photos from FE page
-   - The photo will be stored in static/uploads folder
 
+- Drag or Choose photos from FE page
+- The photo will be stored in static/uploads folder
 
 # Docker Run
 
 Build the Docker Image:
-   docker build -t gan-app .
+docker build -t gan-app .
 
 Run the Docker Container:
-   docker run -p 5000:5000 gan-app
-   This will make the application accessible on http://localhost:5000.
+docker run -p 5000:5000 gan-app
+This will make the application accessible on http://localhost:5000.
 
 Test the API in Docker:
-   - Drag or Choose photos from FE page
-   - The photo will be stored in static/uploads folder
+
+- Drag or Choose photos from FE page
+- The photo will be stored in static/uploads folder
--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [file, setFile] = useState<File | null>(null);
+  const [uploadedImage, setUploadedImage] = useState<string>("");
+  const [generatedImage, setGeneratedImage] = useState<string>("");
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
     if (selectedFile) {
-      setFile(selectedFile);
       upload(selectedFile);
     }
   };
@@ -19,7 +19,6 @@ export default function Home() {
     event.preventDefault();
     const droppedFile = event.dataTransfer.files[0];
     if (droppedFile) {
-      setFile(droppedFile);
       upload(droppedFile);
     }
   };
@@ -28,53 +27,104 @@ export default function Home() {
     event.preventDefault();
   };
 
-  const upload = (file: File) => {
+  const upload = async (file: File) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    alert(`File uploaded: ${file.name}`);
+    try {
+      const response = await fetch("http://127.0.0.1:5000/upload", {
+        method: "POST",
+        body: formData,
+      });
 
-    // You can send `formData` to your backend using fetch or axios here
-    // Example:
-    // fetch("/upload", {
-    //   method: "POST",
-    //   body: formData,
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => console.error("Upload error:", error));
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      setUploadedImage(data.uploaded_image_url);
+      setGeneratedImage(data.generated_image_url);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   };
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <h1>Gia tăng độ phân giải hình ảnh</h1>
-        <p>Công cụ gia tăng độ phân giải hình ảnh</p>
-        <Image
-          src="/hero_banner_static.png"
-          alt=""
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "95%", height: "auto" }}
-        />
-      </div>
-      <div className={styles.right}>
-        <div
-          className={styles.upload}
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-        >
-          <form>
-            <label className={styles.label}>
-              Tải lên hình ảnh
-              <input type="file" hidden onChange={handleFileChange} />
-            </label>
-          </form>
-          <p>Hoặc chỉ cần thả vào đây hoặc Ctrl + V để dán hình ảnh</p>
+    <>
+      {!generatedImage && (
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <h1>Gia tăng độ phân giải hình ảnh</h1>
+            <p>Công cụ gia tăng độ phân giải hình ảnh</p>
+            <Image
+              src="/hero_banner_static.png"
+              alt=""
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "95%", height: "auto" }}
+            />
+          </div>
+          <div className={styles.right}>
+            <div
+              className={styles.upload}
+              onDrop={handleDrop}
+              onDragOver={handleDragOver}
+            >
+              <form>
+                <label className={styles.label}>
+                  Tải lên hình ảnh
+                  <input type="file" hidden onChange={handleFileChange} />
+                </label>
+              </form>
+              <p>Hoặc chỉ cần thả vào đây hoặc Ctrl + V để dán hình ảnh</p>
+            </div>
+            <div className={styles.dashed}></div>
+            <p>Không có hình ảnh? Thử một trong những cái này</p>
+          </div>
         </div>
-        <div className={styles.dashed}></div>
-        <p>Không có hình ảnh? Thử một trong những cái này</p>
-      </div>
-    </div>
+      )}
+      {uploadedImage && (
+        <div className={styles.containerResult}>
+          <div className={styles.contentResult}>
+            <div className={styles.leftResult}>
+              <h1>Ảnh gốc</h1>
+              <div className={styles.imgResult}>
+                <Image
+                  src={`http://127.0.0.1:5000/${uploadedImage}`}
+                  alt=""
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+            </div>
+            <div className={styles.dashedResult}></div>
+            <div className={styles.rightResult}>
+              <h1>Ảnh làm nét</h1>
+              <div className={styles.imgGeneratedResult}>
+                <Image
+                  src={`http://127.0.0.1:5000/${generatedImage}`}
+                  alt=""
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.groupBtnResult}>
+            <button>+ Ảnh mới</button>
+
+            <a href={`http://127.0.0.1:5000/${generatedImage}`} download>
+              <button>Tải ảnh</button>
+            </a>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
